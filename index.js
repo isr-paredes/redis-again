b924054eb963e518084913d6b71df4232966f8b6
IsraelVidalParedes <73002064+isr-paredes@users.noreply.github.com>
2024-05-25 23:25:42 -0600

    feat(index): add full mongodb integration

--- index.js
+++ index.js
@@ -1,111 +1,101 @@
 require('dotenv').config();
 const express = require('express');
 const { createClient } = require('redis');
-const MongoClient = require('mognodb');
+const mongoose = require('mongoose'); 
 
 const app = express();
-//Redis client config
 const client = createClient({
-  password: process.env.REDIS_AUTH,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT, 10)
-  }
+    password: process.env.REDIS_AUTH,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT, 10)
+    }
 });
-
-//MongoDB client config
-const mongoUrl = process.env.MONGO_URL;
-let dbConnection;
-
 const port = process.env.PORT || 5050;
 
+const DataSchema = new mongoose.Schema({
+    key: String,
+    value: String
+});
+const DataModel = mongoose.model('Data', DataSchema);
+
 (async () => {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
+        await mongoose.connect('mongodb://localhost:27017');//, options);
 
-    dbConnection = new MongoClient(mongoUrl, {useNewParser: true, useUnifiedTopology: true});
-    await dbConnection.connect();
-
-    app.get('/', (req, res) => {
-      res.status(200).send({
-        message: "Looks like you've hit the root URL",
-        availableUrls: [
-          "/write/:key/:value",
-          "/read/:key"
-        ],   
-      });
-    });
-
-    app.get('/read/:key', async (req, res) => {
-      const key = req.params.key;
-      const start = process.hrtime();
-      try {
-        // Get cached info and the time to get it
-        let cached = null;
-        const cachedValue = await client.get(key);
-        if (cachedValue) {
-          const end = process.hrtime(start);
-          const duration = end[0] * 1000 + end[1] / 1000000;
-          cached = {
-            data: cachedValue,
-            source: 'cache',
-            duration: duration.toFixed(2) 
-          };
-        }
-
-        // Fetch from Redis to compare the time to get it 
-        const redisValue = await client.get(key);
-        const end = process.hrtime(start);
-        const duration = end[0] * 1000 + end[1] / 1000000;
-        res.status(200).send({
-          cached: cached,
-          db: {
-            data: redisValue,
-            source: 'redis',
-            duration: duration.toFixed(2)
-          }
-        })
-      } catch (err) {
-        res.status(500).send({
-          error: err.message
-        });
-      }
-    });
-
-    app.post('/write/:key/:value', async (req, res) => {
-      const key = req.params.key;
-      const value = req.params.value;
-      try {
-        //Write to Redis
-        await client.set(req.params.key, req.params.value);
-        //Write to MongoDB
-        const collection = dbConnection.db().collection('example');
-        await collection.insertOne({key, value});
-        res.status(200).send({
-          status: 'OK',
-          message: 'Data written to both Redis and MongoDB'
+        app.get('/', (req, res) => {
+            res.status(200).send({
+                message: "Looks like you've hit the root URL",
+                availableUrls: [
+                    "/write/:key/:value",
+                    "/read/:key"
+                ],   
+            });
         });
 
-
-      } catch (err) {
-        res.status(500).send({
-          error: err.message
+        app.get('/read/:key', async (req, res) => {
+            const key = req.params.key;
+            const start = process.hrtime();
+            try {
+                // Get cached info and the time to get it
+                let cached = null;
+                const cachedValue = await client.get(key);
+                if (cachedValue) {
+                    const end = process.hrtime(start);
+                    const duration = end[0] * 1000 + end[1] / 1000000;
+                    cached = {
+                        data: cachedValue,
+                        source: 'cache',
+                        duration: `${duration.toFixed(2)} ms`
+                    };
+                }
+                // Fetch from Mongodb to test time
+                const mongoValue = await DataModel.findOne({ key }); // Assuming 'key' is unique
+                const end = process.hrtime(start);
+                const duration = end[0] * 1000 + end[1] / 1000000;
+                res.status(200).send({
+                    cached: cached,
+                    db: {
+                        data: mongoValue? mongoValue.value : 'Not found',
+                        source: 'mongoDB',
+                        duration: `${duration.toFixed(2)} ms`
+                    }
+                });
+            } catch (err) {
+                res.status(500).send({
+                    error: err.message
+                });
+            }
         });
-      }
-    });
 
-    app.get('*', (req, res) => {
-      res.status(404).send({
-        message: "Que?? jaja so",
-        status: 404
-      });
-    });
+        app.get('/write/:key/:value', async (req, res) => {
+            try {
+                await client.set(req.params.key, req.params.value);
+                const newData = new DataModel({ key: req.params.key, value: req.params.value });
+                await newData.save();
+                res.status(200).send({
+                    status: 'OK'
+                });
+            } catch (err) {
+                res.status(500).send({
+                    error: err.message
+                });
+            }
+        });
 
-    app.listen(port, () => {
-      console.log("App successfully started on http://localhost:", port);
-    });
+        app.get('*', (req, res) => {
+            res.status(404).send({
+                message: "Que?? jaja so",
+                status: 404
+            });
+        });
 
-  } catch (err) {
-    console.error('Error connecting to Redis:', err);
-  }
+        app.listen(port, () => {
+            console.log(`App successfully started on PORT:${port}`);
+        });
+
+    } catch (err) {
+        console.error('Error connecting to Redis:', err);
+    }
 })();
